a7e51a9fb8668c6e9a03a77fcfbc85c06922e716
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2024-06-11 18:36:17 +0200

    created variables for arithmatic operations

--- variable.js
+++ variable.js
@@ -1,6 +1,19 @@
 let name = 'Reabetswe';
-let age = '23';
+let age = 23;
 let isStudent = true;
-if(isStudent){
-    console.log('Reabetswe is a student')
-}
+console.log(name, age, isStudent);
+
+let favoriteColour = 'pink';
+console.log(favoriteColour);
+
+favoriteColour = "black";
+console.log(favoriteColour);
+
+let num1 = 33;
+let num2 = 18;
+
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1/ num2;
+console.log(sum, difference, product, quotient);
