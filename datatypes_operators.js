23c1c1db1b523f0524ad22dce0d2c128d3f1f8a6
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2024-06-12 21:48:37 +0200

    created variables for data typs

--- datatypes_operators.js
+++ datatypes_operators.js
@@ -1,2 +1,8 @@
 let number3 = 50;
 let number4 = 23.3;
+
+let addition = number3 + number4;
+let subtraction = number3 - number4;
+let multiplication = number3 *number4;
+let division = number3 / number4;
+let modulus = number3 % number4;
