38a9abeb5fe38c83b2ac29718839cba3607d9251
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2024-06-12 22:12:35 +0200

    printed variables for data types

--- datatypes_operators.js
+++ datatypes_operators.js
@@ -6,3 +6,6 @@ let subtraction = number3 - number4;
 let multiplication = number3 *number4;
 let division = number3 / number4;
 let modulus = number3 % number4;
+let exponentiation = number3 ** number4;
+
+console.log(addition, subtraction, multiplication, division, modulus, exponentiation);
