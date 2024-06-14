31c20eb5f3480040d1b17f56e88fbadb0936c306
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2024-06-14 23:09:45 +0200

    created IF statements

---
+++ conditionalStatements_loops.js
@@ -0,0 +1,13 @@
+let temp = 7;
+
+if(temp < 0);
+console.log("It's freezing");
+
+if(temp = 0 && temp <=15);
+console.log("It's cold");
+
+if(temp = 16 && temp <=25);
+console.log("It's mild");
+
+if(temp >25);
+console.log("It's warm");
