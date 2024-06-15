33155160eeb588551c1aec0900624dd570f1e222
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2024-06-16 00:40:41 +0200

    created switch statements

--- conditionalStatements_loops.js
+++ conditionalStatements_loops.js
@@ -11,3 +11,14 @@ console.log("It's mild");
 
 if(temp >25);
 console.log("It's warm");
+
+switch(temp){
+    case temp<0:
+        console.log("It's freezing");
+    case temp = 0 && temp <=15:
+            console.log("It's cold");
+    case temp = 16 && temp <=25:
+        console.log("It's mild");
+    case temp >25:
+        console.log("It's warm");
+}
